4c57bd559e50de3ccb13e44e5ed84615fb588db2
mengerya <30553694+mengerya@users.noreply.github.com>
2017-09-30 06:53:11 +0800

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -241,3 +241,63 @@ pNode JosephCircle(pNode pHead, size_t M)
 	pCur->_pNext = NULL;
 	return pCur;
 }
+
+
+// 单链表的逆置--前后指针 
+pNode ReverseList_1(pNode pHead)
+{
+	pNode  pPre;
+	pNode  pCur;
+	pNode  pNext;
+	if(NULL == pHead || NULL == pHead->_pNext)
+		return NULL;
+	pPre = pHead;
+	pCur = pPre->_pNext;
+	pNext = pCur->_pNext;
+	pHead->_pNext = NULL;
+	while(pNext)
+	{
+		pCur->_pNext = pPre;
+		pPre = pCur;
+		pCur = pNext;
+		pNext = pCur->_pNext;
+	}
+	pCur->_pNext= pPre;
+	return pCur;
+}
+
+// 单链表的逆置--头插法 
+pNode ReverseList_2(pNode pHead)
+{
+	pNode pPre = NULL;
+	pNode pCur = NULL;
+	pNode pNewHead = NULL;
+	if(NULL == pNewHead)
+		return NULL;
+	pPre = pHead;
+	pCur = pHead->_pNext;
+	while(pCur)
+	{
+		pPre->_pNext = pNewHead;
+		pNewHead = pPre;
+		pPre = pCur;
+		pCur = pPre->_pNext;
+	}
+	pPre->_pNext = pNewHead;
+	pNewHead = pPre;
+	return pNewHead;
+}
+// 查找链表的中间结点---要求不能遍历单链表 
+pNode FindMidNode(pNode pHead)
+{
+	pNode pSlow = pHead;
+	pNode pFast = pHead;
+	if(NULL == pHead)
+		return NULL;
+	while(pFast && pFast->_pNext)
+	{
+		pSlow = pSlow->_pNext;
+		pFast = pFast->_pNext->_pNext;
+	}
+	return pSlow;
+}
